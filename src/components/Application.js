bfc3e37b785fb382a0a12601edf647d1947d6aa3
Andy Junhyuk Park <pos03142ap@gmail.com>
2022-01-19 15:23:53 -0800

    refactor and update InterviewerList.js && InterviewerListItem.js && stories/index.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import DayList from "components/DayList";
+import InterviewerList from "./InterviewerListItem";
 import "components/Application.scss";
 
 const days = [
