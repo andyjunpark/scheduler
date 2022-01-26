2971eadbefd15b1059cd7210758f6cd56a292ff1
Andy Junhyuk Park <pos03142ap@gmail.com>
2022-01-26 15:39:09 -0800

    add error when student name is blank using fireEvent helper

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import InterviewerList from 'components/InterviewerList';
 export default function Form(props) {
 const [student, setStudent] = useState(props.student || "");
 const [interviewer, setInterviewer] = useState(props.interviewer || null);
+const [error, setError] = useState("");
 
 const reset = () => {
   setStudent("") 
@@ -15,6 +16,15 @@ const cancel = () => {
   reset();
   props.onCancel();
 }
+
+function validate() {
+  if (student === "") {
+    setError("Student name cannot be blank");
+    return;
+  }
+  props.onSave(student, interviewer);
+}
+
 return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -28,7 +38,8 @@ return (
           onChange={(event) => setStudent(event.target.value)}
           value={student}
         />
-      </form>
+        </form>
+      <section className="appointment__validation">{error}</section>
       <InterviewerList
         interviewers={props.interviewers}
         value={interviewer}
@@ -38,7 +49,7 @@ return (
       <section className="appointment__card-right">
           <section className="appointment__actions">
               <Button danger onClick={cancel}>Cancel</Button>
-              <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+              <Button confirm onClick={() => validate()}>Save</Button>
           </section>
       </section>
     </main>
